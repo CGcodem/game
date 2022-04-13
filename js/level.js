774db5e908b682d4fbeb57b26668678c6a34703d
MonsterCT <paolocosmo@icloud.com>
2022-04-13 10:53:48 +0200

    fixed levels
    changed "no go places"

--- js/level.js
+++ js/level.js
@@ -6,18 +6,30 @@ const app = {
       isDed: false,
       gameStatus: 'game',
       noGoPlaces: [{
-        x: 3,
-        y:3,
+        x: 2,
+        y:0,
       },{
         x: 1,
+        y:1,
+      },{
+        x: 3,
         y:2,
       },{
+        x: 1,
+        y:3,
+      },{
+        x: 2,
+        y:3,
+      },{
+        x: 2,
+        y:4,
+      },{
         x: 4,
-        y:2,
+        y:4,
       }],
       goalPosition: {
-        x: 2,
-        y: 2
+        x: 3,
+        y: 4
       },
       character: {
         canMove: true,
